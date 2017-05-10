b72172849fc8b6b469fcaf13d7c57d209dc01a59
WendyMin <min711s9d31@126.com>
2017-05-10 16:44:44 +0800

    add 写入txt文档，网址转换

--- 265.py
+++ 265.py
@@ -10,11 +10,32 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
+# 写入txt文档
+def WriteFile(content):
+	with open('265.txt', 'a') as f:
+		f.write(content)
+		f.write("\n")
+
 # 用BeautifulSoup指定lxml解析器解析网页，得到一个文档的对象
 def getPage(forumUrl):
 	articleHtml = urlopen("http://www.265.com/"+forumUrl)
 	articleBsObj = BeautifulSoup(articleHtml, "lxml")
 	return articleBsObj
+
+# 网址转换
+def checkUrl(url):
+	url = url.replace("%3A", ":")
+	url = url.replace("%2F", "/")
+	if re.search(re.compile("www."), url):
+		a = url.split("www.")
+		urltemp = "www." + a[len(a) - 1]
+	else:
+		if re.search(re.compile("http"), url):
+			a = url.split("http")
+			urltemp = "http" + a[len(a) - 1]
+	if urltemp.endswith("/"):
+		urltemp = urltemp[0:len(urltemp)-1]
+	return urltemp
 
 # 打印网站分类，各网站的名称和链接
 def printWebSite(somethings):
@@ -22,16 +43,22 @@ def printWebSite(somethings):
 		onethingUrl = onething.attrs["href"]
 		onethingObj = getPage(onethingUrl)
 		print(onething.get_text(), onethingUrl)
+		WriteFile(onething.get_text()+" "+onethingUrl)
 		webPages = onethingObj.findAll("span", {"class": "b-f-j"})
 		for webPage in webPages:
 			if not re.search(re.compile(r"相关搜索|分类目录"), str(webPage)):		# 不包含相关搜索或分类目录
 				print(webPage.get_text())
+				WriteFile(webPage.get_text())
 				page1 = webPage.parent.parent.nextSibling.a.parent
-				print(page1.a.get_text(), ":", page1.a.attrs["href"])
+				# print(page1.a.get_text(), ":", page1.a.attrs["href"])
+				print(page1.a.get_text(), ":", checkUrl(page1.a.attrs["href"]))
+				WriteFile(page1.a.get_text()+":"+page1.a.attrs["href"])
 				pageNext = page1.nextSibling
 				while pageNext != None:
 					if re.search(re.compile("^(http)"), pageNext.a.attrs["href"]):  # 天气->湛江 的网址不对
-						print(pageNext.get_text(), ":", pageNext.a.attrs["href"])
+						# print(pageNext.get_text(), ":", pageNext.a.attrs["href"])
+						print(pageNext.get_text(), ":", checkUrl(pageNext.a.attrs["href"]))
+						WriteFile(pageNext.get_text()+":"+pageNext.a.attrs["href"])
 					pageNext = pageNext.nextSibling
 
 # 生活服务分类
@@ -39,6 +66,7 @@ def Life():
 	life = bsObj.find("span", text="生活服务")
 	services = life.parent.parent.nextSibling.findAll("a", {"site-c-action": "c-category-life-click"})
 	print("【生活服务类】")
+	WriteFile("【生活服务类】")
 	printWebSite(services)
 
 # 休闲娱乐分类
@@ -46,6 +74,7 @@ def Relax():
 	relax = bsObj.find("span", text="休闲娱乐")
 	entertainments = relax.parent.parent.nextSibling.findAll("a", {"site-c-action": "c-category-relax-click"})
 	print("【休闲娱乐类】")
+	WriteFile("【休闲娱乐类】")
 	printWebSite(entertainments)
 
 # 其他分类
@@ -53,6 +82,7 @@ def Other():
 	other = bsObj.find("span", text="其他")		# 找到其他分类
 	otherthings = other.parent.parent.nextSibling.findAll("a", {"site-c-action": "c-category-other-click"})
 	print("【其他】")
+	WriteFile("【其他】")
 	printWebSite(otherthings)
 
 
@@ -72,3 +102,4 @@ if __name__ == '__main__':
 			Relax()
 		if int(choice1) == 3:
 			Other()
+
